dd06d4d1ea03d067c4472f922a3e3adf431b7c3a
Nikita <main.acr0matic@gmail.com>
2021-01-28 22:20:34 +0300

    feat: исправления, доработка мобильного меню

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -133,6 +133,7 @@ function ResetSelect(select) {
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuOpen = document.querySelector('.header__mobile-button');
 const mobileMenuClose = mobileMenu.querySelector('.mobile-menu__close');
+const mobileMenuItems = mobileMenu.querySelectorAll('.mobile-menu__item');
 
 mobileMenuOpen.addEventListener('click', () => {
   mobileMenu.classList.toggle('mobile-menu--open')
@@ -141,3 +142,41 @@ mobileMenuOpen.addEventListener('click', () => {
 mobileMenuClose.addEventListener('touchend', () => {
   mobileMenu.classList.toggle('mobile-menu--open')
 });
+
+mobileMenuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-menu--open')
+  });
+});
+
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+
+scrollTo();
+
+function scrollTo() {
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+      link.onclick = scrollAnchors;
+    }
+  }
+}
+
+function scrollAnchors(e, respond = null) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+  const checkIfDone = setInterval(function () {
+    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      window.history.pushState('', '', targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
